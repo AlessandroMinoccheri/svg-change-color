b8b7167f0e9d22ecf598bb361808ec62fdbe827c
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-03-14 11:29:24 +0100

    added js file

---
+++ js/jquery.svgChangeColor.js
@@ -0,0 +1,25 @@
+/*
+Alessandro Minoccheri
+V 0.0.1
+14-03-2015
+*/
+
+(function ($) {
+	var numOptions = 0;
+
+    $.fn.extend({      
+        svgChangeColor: function (argumentOptions) {
+            var defaults = {
+				
+			}
+			
+			var options = $.extend(defaults, argumentOptions);
+			
+			return this.each(function () {
+                var o 					= options;
+				var obj 				= $(this);
+				
+            });
+        }
+    });
+})(jQuery);
